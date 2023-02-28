f4e5754e3ae08304cd6c9da44203b5e5a3e0803a
Santi <antratocx@gmail.com>
2023-02-28 21:18:14 +0100

    staged

--- TrabajoPractico.java
+++ TrabajoPractico.java
@@ -9,28 +9,24 @@ public class ArgPro4 {
         int ingresos = 739, sup = 31, energia = 3331;
         if (ingresos < 738 && sup < 30 && energia < 3330){
     }
-    
-        
-        
-        
-//        if (ingresos < 748){
-//            System.out.println("Subsidiado");
-//        } 
-//        else {
-//            System.out.println("No subsidiado");
-//        }
-//        if (sup < 30){
-//            System.out.println("Tiene subsidio de rentas");
-//        }
-//        else {
-//            System.out.println("No tiene subsidio de rentas");
-//        }
-//        if (energia < 3330){
-//            System.out.println("Tiene subsidio de energia");
-//        }
-//        else{
-//            System.out.println("No tiene subsidio de energia");
-//        }
+        if (ingresos < 748){
+            System.out.println("Subsidiado");
+        } 
+        else {
+            System.out.println("No subsidiado");
+        }
+        if (sup < 30){
+            System.out.println("Tiene subsidio de rentas");
+        }
+        else {
+            System.out.println("No tiene subsidio de rentas");
+        }
+        if (energia < 3330){
+            System.out.println("Tiene subsidio de energia");
+        }
+        else{
+            System.out.println("No tiene subsidio de energia");
+        }
         
         
         
